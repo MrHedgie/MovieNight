75de39b8cc5887d7b085d8bdf22ade952a61b8d1
Adam <madaoksap@gmail.com>
2025-03-16 22:36:21 +0100

    Initial Commit

---
+++ src/main/java/com/Hedgehogs/MovieNight/MovieNightApplication.java
@@ -0,0 +1,13 @@
+package com.Hedgehogs.MovieNight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieNightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieNightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Hedgehogs/MovieNight/MovieNightApplicationTests.java
@@ -0,0 +1,9 @@
+package com.Hedgehogs.MovieNight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieNightApplicationTests {
+
+}
